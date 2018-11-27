56d771632933b7ce0903e51266eaef2d12e1967f
github username <github email address>
2018-11-27 15:45:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(array)
   # code here
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+
+    array
+  else
+    "This block should not run!"
+  end
+end
+
+my_each(words) do |word|
+  word
 end
